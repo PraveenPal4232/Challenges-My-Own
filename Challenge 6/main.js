33667337e87d2a18c0245f592ed24a3b8cdd2f55
Praveen Pal <praveenpal4232@gmail.com>
2019-10-05 22:39:32 +0530

    update

--- Challenge 6/main.js
+++ Challenge 6/main.js
@@ -12,10 +12,12 @@ else{
 document.getElementById("lavel1").style.display = "block";
 document.getElementById("btn1").addEventListener("click", myfunction1);
 
+var ff1 = document.getElementsByClassName("f1");
+
 function myfunction1(){
-    if(){
-        
+    if(ff1[0] .value !== ""){
+        document.getElementById("lavel1").style.display = "none";
+        document.getElementById("lavel2").style.display = "block";
     }
-    document.getElementById("lavel1").style.display = "none";
-    document.getElementById("lavel2").style.display = "block";
+    
 }
